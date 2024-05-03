f0d809adef75c90983199439fda2c8db3726de07
BSM-777 <137193645+BSM-777@users.noreply.github.com>
2024-05-04 00:14:59 +0100

    New Update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Fabric script distributes an archive to the web servers"""
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.157.185.32', '100.26.229.178']
+env.hosts = ['54.157.185.32', '	100.26.229.178']
 
 
 def do_deploy(archive_path):
@@ -23,5 +26,5 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
+    except:
         return False
